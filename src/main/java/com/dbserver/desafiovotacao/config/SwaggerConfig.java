8e340d1ddc50f1123a84f3297c967da708ab9c9b
julia.mossellim <julia.mossellim@dbserver.com.br>
2024-12-12 23:21:37 -0300

    Adiciona estrutura básica,dados da conexão com o banco e configuração do Swagger

---
+++ src/main/java/com/dbserver/desafiovotacao/config/SwaggerConfig.java
@@ -0,0 +1,20 @@
+package com.dbserver.desafiovotacao.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Slf4j
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Desafio Votação API")
+                        .description("API desenvolvida para desafio votação da DBServer."));
+    }
+}
